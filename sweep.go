4d76098fe40c36763f4a71f61d1d5b5e9c9432da
Chris Skilton <chriss@brandwatch.com>
2018-06-15 14:02:13 +0100

    go implementation of sweepstake generator

---
+++ sweep.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+)
+
+func checkError(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	peopleJSONPathPtr := flag.String("people", "", "Path to JSON array of names")
+	teamsJSONPathPtr := flag.String("teams", "", "Path to JSON array of teams")
+
+	flag.Parse()
+
+	if len(*peopleJSONPathPtr) == 0 {
+		panic("must supply --people option. run sweeps --help if you are stuck")
+	}
+
+	if len(*teamsJSONPathPtr) == 0 {
+		panic("must supply --teams option. run sweeps --help if you are stuck")
+	}
+
+	peopleJSON, err := ioutil.ReadFile(*peopleJSONPathPtr)
+	checkError(err)
+	teamsJSON, err := ioutil.ReadFile(*teamsJSONPathPtr)
+	checkError(err)
+
+	var people []string
+	var teams []string
+	var slicedPeople []string
+
+	json.Unmarshal(peopleJSON, &people)
+	json.Unmarshal(teamsJSON, &teams)
+
+	picks := make(map[string][]string)
+
+	for i := 0; i < len(teams); i++ {
+		if len(slicedPeople) == 0 {
+			slicedPeople = make([]string, len(people))
+			copy(slicedPeople, people)
+		}
+
+		team := teams[i]
+		personIndex := rand.Intn(len(slicedPeople))
+		person := slicedPeople[personIndex]
+
+		fmt.Print("assigning team " + team + " to person " + person + "\n")
+
+		picks[person] = append(picks[person], team)
+		slicedPeople = append(slicedPeople[:personIndex], slicedPeople[personIndex+1:]...)
+	}
+
+	outputJson, err := json.MarshalIndent(picks, "", " ")
+	checkError(err)
+	fmt.Print(string(outputJson))
+}
